60f37b476bcc02fb2bd927861f86ab1a4a503a56
a9020 <ives.he@hyweb.com.tw>
2021-11-18 14:38:29 +0800

    init: new project
    1. 建立新專案 股票顯示小工具

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <StockSelect msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import StockSelect from '@/components/StockSelect.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    StockSelect
+  },
+  methods: {
+
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/StockSelect.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <h2>{{ msg }}</h2>
+    <a-select
+      show-search
+      placeholder="input search text"
+      style="min-width: 20em"
+      @change="selected"
+      :filterOption="filter"
+    >
+      <a-select-option
+        v-for="([k, v], idx) in Object.entries(stocks)"
+        :key="idx"
+        :title="v"
+        :value="k"
+      >
+        {{ `${k} ${v}` }}
+      </a-select-option>
+    </a-select>
+    <a-button type="primary" @click="getStock" :disabled="disabled">{{
+      "Search"
+    }}</a-button>
+  </div>
+</template>
+
+<script>
+import stocks from '@/assets/twStocks.json'
+import axios from 'axios'
+
+export default {
+  name: 'StockSelect',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      stocks,
+      selectedId: '',
+      disabled: false
+    }
+  },
+  emits: ['selectedId'],
+  methods: {
+    filter(input, option) {
+      return option.title.includes(input) || option.value.includes(input)
+    },
+    selected(val) {
+      this.selectedId = val
+    },
+    async getStock() {
+      if (this.selected != '') {
+        if (!this.disabled) {
+          this.disabled = true
+          await this.connectTwse()
+          await new Promise(resolve => setTimeout(resolve, 5000));
+          this.disabled = false
+        }
+      }
+    },
+    async connectTwse() {
+      const key = this.selectedId
+      const { data } = await axios.get(`https://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=tse_${key}.tw&json=1&delay=0`)
+      const [{ o: open, n: name, z: trade, h: high, l: low }] = data.msgArray
+      const result = [
+        `${key} ${name}`,
+        `成交價 ${trade}`,
+        `開盤 ${open}`,
+        `最高 ${high}`,
+        `最低 ${low}`
+      ]
+      console.log(result)
+    },
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
